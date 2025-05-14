ef72b837d757837a69383c23df3ea333f3cdaca8
CJ <phillgilmartin@gmail.com>
2025-05-14 10:08:01 -0700

    Update $id.tsx
    Revert id.tsx

--- app/routes/$id.tsx
+++ app/routes/$id.tsx
@@ -1,71 +1,54 @@
 import {
   type LoaderFunctionArgs,
   type ActionFunctionArgs,
-  json,
-  redirect,
 } from "@remix-run/cloudflare";
 import { useLoaderData, Form } from "@remix-run/react";
 import { TodoManager } from "~/to-do-manager";
 
 export const loader = async ({ params, context }: LoaderFunctionArgs) => {
-  const { id } = params;
-  if (!id) {
-    throw new Response("Missing list ID", { status: 400 });
-  }
-
-  const key = `todos:${id}`;
-  const manager = new TodoManager(context.env.TO_DO_LIST, key);
-  const todos = await manager.list();
-  return json({ todos });
+  const todoManager = new TodoManager(
+    context.cloudflare.env.TO_DO_LIST,
+    params.id,
+  );
+  const todos = await todoManager.list();
+  return { todos };
 };
 
 export async function action({ request, context, params }: ActionFunctionArgs) {
-  const { id } = params;
-  if (!id) {
-    throw new Response("Missing list ID", { status: 400 });
-  }
-
-  const key = `todos:${id}`;
-  const manager = new TodoManager(context.env.TO_DO_LIST, key);
+  const todoManager = new TodoManager(
+    context.cloudflare.env.TO_DO_LIST,
+    params.id,
+  );
   const formData = await request.formData();
   const intent = formData.get("intent");
 
   switch (intent) {
     case "create": {
       const text = formData.get("text");
-      if (typeof text !== "string" || !text) {
-        return json({ error: "Invalid text" }, { status: 400 });
-      }
-      await manager.create(text);
-      break;
+      if (typeof text !== "string" || !text)
+        return Response.json({ error: "Invalid text" }, { status: 400 });
+      await todoManager.create(text);
+      return { success: true };
     }
 
     case "toggle": {
-      const todoId = formData.get("id");
-      if (typeof todoId !== "string") {
-        return json({ error: "Invalid ID" }, { status: 400 });
-      }
-      await manager.toggle(todoId);
-      break;
+      const id = formData.get("id") as string;
+      await todoManager.toggle(id);
+      return { success: true };
     }
 
     case "delete": {
-      const todoId = formData.get("id");
-      if (typeof todoId !== "string") {
-        return json({ error: "Invalid ID" }, { status: 400 });
-      }
-      await manager.delete(todoId);
-      break;
+      const id = formData.get("id") as string;
+      await todoManager.delete(id);
+      return { success: true };
     }
 
     default:
-      return json({ error: "Invalid intent" }, { status: 400 });
+      return Response.json({ error: "Invalid intent" }, { status: 400 });
   }
-
-  return redirect(`/${id}`);
 }
 
-export default function TodoRoute() {
+export default function () {
   const { todos } = useLoaderData<typeof loader>();
 
   return (
